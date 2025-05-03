ca1e3ac82cc7021982b6f480c2e16880ad5cbeb9
virginity-construct <pjv.melb@gmail.com>
2025-05-03 22:11:53 +1000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+import tailwindcss from '@tailwindcss/postcss'
+
+export default {
   plugins: {
-    tailwindcss: {},
+    [tailwindcss]: {},
     autoprefixer: {},
   },
-};
+}
